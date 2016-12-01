00072e070ea7eda0260148b96913006ce45a909a
Yuki Tetsuka <yuki@Yuki-no-MacBook-Air.local>
2016-12-01 09:35:20 +0900

    http requestを受け付ける

--- app/index.js
+++ app/index.js
@@ -40,3 +40,10 @@ wsServer.on('connection', ws => {
 
   });
 });
+
+var http = require('http');
+http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('Hello, World');
+  res.end();
+}).listen(8080);
