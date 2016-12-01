1f48ff22c08f65a680ba2c18f31df0308e4acf77
Yuki Tetsuka <yuki@Yuki-no-MacBook-Air.local>
2016-12-01 09:44:11 +0900

    ポート指定部分を修正

--- app/index.js
+++ app/index.js
@@ -46,4 +46,4 @@ http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.write('Hello, World');
   res.end();
-}).listen(8080);
+}).listen(process.env.PORT || 8080);
