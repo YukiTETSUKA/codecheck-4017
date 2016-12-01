d50b440a1bef65b31b522b2dced94d6ba2520441
Yuki Tetsuka <yuki@Yuki-no-MacBook-Air.local>
2016-12-01 19:12:14 +0900

    イベント情報を取得し，broadcastする

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const server   = express()
   .use((req, res) => res.send('Hello :D'))
   .listen(process.env.PORT || port);
 var wsServer   = new require('ws').Server({server});
+const moment   = require('moment');
 
 wsServer.broadcast = msg => {
   wsServer.clients.forEach(client => {
@@ -29,19 +30,64 @@ wsServer.on('connection', ws => {
         (reg_result = req.text.match(/^(?:bot)(?:\s|　)+(.*)/ )) != null || /* bot  xxx */
         (reg_result = req.text.match(/^(?:bot:)(?:\s|　)*(.*)/)) != null) { /* bot: xxx */
 
+      reg_result.shift();
+
       res.type = 'bot';
-      switch (reg_result[reg_result.length - 1]) {
+
+      var tmp     = reg_result.unshift().match(/^\S+ (.*)/);
+      var command = tmp.unshift();
+      var params  = tmp.length == 0 ? null : tmp.unshift();
+      switch (command) {
         case 'ping':
           res.text = 'pong';
+          wsServer.broadcast(res);
           break;
+
+        case 'event': /* 今月に開かれるイベントを紹介する */
+          var splited_params = params.split(/(\s|　)+/);
+          getEvents(splited_params, res);
 
         default:
           res.text = 'Oops!:O';
+          wsServer.broadcast(res);
       }
+    } else {
+      wsServer.broadcast(res);
     }
-
-    wsServer.broadcast(res);
   });
 });
 
 console.log('Express server listening on port: %d', server.address().port);
+
+/*
+ * https://eventon.jp/api/events.json からイベント情報を取得し，戻り値とする
+ * params[0] : 都道府県名
+ * params[1] : キーワード1
+ * params[2] : キーワード2 ...
+*/
+const prefectures = [
+  '北海道', '青森県'  , '岩手県', '宮城県', '秋田県', '山形県'  , '福島県', '茨城県', '栃木県', '群馬県'  , '埼玉県', '千葉県',
+  '東京都', '神奈川県', '新潟県', '富山県', '石川県', '福井県'  , '山梨県', '長野県', '岐阜県', '静岡県'  , '愛知県', '三重県',
+  '滋賀県', '京都府'  , '大阪府', '兵庫県', '奈良県', '和歌山県', '鳥取県', '島根県', '岡山県', '広島県'  , '山口県', '徳島県',
+  '香川県', '愛媛県'  , '高知県', '福岡県', '佐賀県', '長崎県'  , '熊本県', '大分県', '宮崎県', '鹿児島県', '沖縄県'
+];
+const http     = require('http');
+const endpoint = 'https://eventon.jp/api/events.json';
+function getEvents(params, res) {
+  var prefecture_id = prefectures.indexOf(params.shift() || '東京都') + 1;
+  var keyword       = params.length == 0 ? '' : params.join();
+  var ymd_between   = `${moment().add(1, 'days').format('YYYYMMDD')},${moment().add(31, 'days').format('YYYYMMDD')}`; // 開催年月日範囲
+  const url = `${endpoint}?prefecture_id=${prefecture_id}&keyword=${keyword}&ymd_between=${ymd_between}&order=started_at_asc`;
+
+  if (pref_id == 0) { // 都道府県名がhitしなかった場合，空配列を返却
+    res.text = 'Oops! :O Prefecture not found XO';
+    wsServer.broadcast(res);
+  }
+
+  http.get(url, eventon_res => {
+    eventon_res.setEncoding('utf8');
+    res.text = JSON.stringify(JSON.parse(eventon_res).events);
+
+    wsServer.broadcast(res);
+  });
+}
