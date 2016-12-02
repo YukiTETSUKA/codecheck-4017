db13aa3d8b7ffc614effc23a73c091491c07859f
Yuki Tetsuka <yuki@Yuki-no-MacBook-Air.local>
2016-12-02 12:17:32 +0900

    slickを導入し，イベント情報をカルーセルで表示

--- app/client.js
+++ app/client.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var ws = new WebSocket('wss://codecheck-4017.herokuapp.com:443/');
-// var ws = new WebSocket('ws://localhost:5000/');
+// var ws = new WebSocket('wss://codecheck-4017.herokuapp.com:443/');
+var ws = new WebSocket('ws://localhost:5000/');
 
 $(function () {
   $('form').submit(function(){
@@ -22,21 +22,25 @@ $(function () {
 
   ws.onmessage = function(msg){
     var resp = JSON.parse(msg.data);
-    if (resp.events) { // イベント情報があればそれを表示
+    $('#messages')
+      .append($('<li>')
+      .append($('<span class="message">').text(resp.text)));
+
+    if (resp.events && !(resp.events.length == 0)) { // イベント情報があればそれを表示
+      var slick_container = $('<div class="multiple-items">');
 
       resp.events.forEach(e => {
-        $('#messages')
-          .append($('<li>')
-          .append($('<span class="message">')
-            .append($('<a href="' + e.url + '">').text(e.title))));
+        slick_container.append($('<div class="slick-slide">')
+          .append($('<a href="' + e.url + '">')
+            .append($('<img class="slick-slide" src="' + e.image_path + '" alt="' + e.title + '">')).append(e.title)));
       });
 
-    } else {
-
-      $('#messages')
-        .append($('<li>')
-        .append($('<span class="message">').text(resp.text)));
-
+      $('#messages').append($('<li>').append(slick_container));
+      slick_container.slick({
+        infinite      : true,
+        slidesToShow  : 3,
+        slidesToScroll: 3
+      });
     }
   };
 
